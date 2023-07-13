a52f45853f8476fb9875a8d599708d79a019637e
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2023-07-13 16:29:40 +0300

    add asset image gen
    __

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,19 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesProfile
+  /// assets/images/profile.jpg
+  static const String imagesProfile = "assets/images/profile.jpg";
+
+  /// Assets for imagesSvgImagesIllustration
+  /// assets/images/svg-images/illustration.svg
+  static const String imagesSvgImagesIllustration = "assets/images/svg-images/illustration.svg";
+
+  /// Assets for imagesSvgImagesIllustration1
+  /// assets/images/svg-images/illustration1.svg
+  static const String imagesSvgImagesIllustration1 = "assets/images/svg-images/illustration1.svg";
+
+  /// Assets for imagesSvgImagesIllustration2
+  /// assets/images/svg-images/illustration2.svg
+  static const String imagesSvgImagesIllustration2 = "assets/images/svg-images/illustration2.svg";
+}
