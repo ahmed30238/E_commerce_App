150ac4f1764b66695ab8a1e6abc6eb264449a9bf
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2023-08-24 15:19:09 +0300

    image pickers

--- lib/core/helper_methods/helper_methods.dart
+++ lib/core/helper_methods/helper_methods.dart
@@ -1,5 +1,8 @@
+import 'package:e_commerce_app/core/helper_functions.dart';
 import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/cupertino.dart';
 
 navigateAndRemove({required BuildContext context, required String path}) {
   Navigator.pushNamedAndRemoveUntil(context, path, (route) => false);
@@ -10,3 +13,30 @@ navigateTo({required BuildContext context, required String path}) {
 }
 
 SharedPreferences? prefs;
+
+Future<void> showImagePicker({
+  required BuildContext context,
+  XFile? cameraImage,
+  VoidCallback? onGalleryTapped,
+  VoidCallback? onCameraTapped,
+}) async {
+  showCupertinoModalPopup(
+    context: context,
+    builder: (context) => CupertinoActionSheet(
+      actions: [
+        CupertinoButton(
+          onPressed: onGalleryTapped,
+          child: const Text("Gallery"),
+        ),
+        CupertinoButton(
+          onPressed: onCameraTapped,
+          child: const Text("Camera"),
+        ),
+      ],
+      cancelButton: CupertinoButton(
+        child: const Text("cancel"),
+        onPressed: () => Navigator.pop(context),
+      ),
+    ),
+  );
+}
